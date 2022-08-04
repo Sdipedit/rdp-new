bf550eadbf0dcf6ce36b8f1da3459ad16030dcee
sdip <76566936+Sdipedit@users.noreply.github.com>
2022-08-04 11:21:44 +0530

    Delete wallpaper.bat

--- wallpaper.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarSmallIcons /t REG_DWORD /d 1 /f
-REG ADD "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d D:\a\wallpaper.jpg /f
-@D:\a\R3yy-RDP\R3yy-RDP\RULES.txt
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
-taskkill /f /im explorer.exe
-start explorer.exe
